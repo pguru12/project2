3235ae01bad116eaf0877c0c00ebb51283af579f
Pranav <pguru12@gmail.com>
2017-06-07 09:36:33 -0400

    List is working

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ function submitHander(ev){
     const f = ev.target
     const details=document.querySelector('#details')
     const movie = f.cageMovies.value
+    const movie2 = f.anotherMovie.value
 
-    const boldName = document.createElement('strong')
-    boldName.textContent = name
+   // const boldName = document.createElement('strong')
+   // boldName.textContent = name
 
-    details.appendChild(boldName)
+   // details.appendChild(boldName)
+   details.innerHTML=`
+   <ul>
+    <li>Name: ${movie}</li>
+    <li>Name: ${movie2}</li>
+   </ul>`
 }
 
 cageMovies.addEventListener('submit', submitHander)
