b2526c3eb1705f0befd5eeef00725d2b53de249f
Pranav <pguru12@gmail.com>
2017-06-07 11:55:09 -0400

    Adding stuff

--- index.js
+++ index.js
@@ -5,27 +5,35 @@ function submitHander(ev){
     const f = ev.target
     const details=document.querySelector('#details')
     const movie = f.cageMovies.value
-    const movie2 = f.anotherMovie.value
 
     const boldName = document.createElement('strong')
     boldName.textContent = name
    const nameItem = document.createElement('li')
    nameItem.textContent = `Favorite Movie: ${movie}`
 
-   const nameItem2 = document.createElement('li')
-   nameItem2.textContent = `Second Favorite Movie: ${movie2}`
-
    const list = document.createElement('ul')
    list.appendChild(nameItem)
-   list.appendChild(nameItem2)
 
    details.appendChild(list)
-   //list.appendChild(boldName)
-  // details.innerHTML=`
-  // <ul>
-//     <li>Movie: ${movie}</li>
-//     <li>Movie: ${movie2}</li>
-//    </ul>`
+   details.insertBefore(list, nameItem)
+}
+
+function submitHandle(ev){
+    ev.preventDefault()
+    const f = ev.target
+    const details=document.querySelector('#details')
+    const movie = f.cageMovies.value
+
+    const boldName = document.createElement('strong')
+    boldName.textContent = name
+   const nameItem = document.createElement('li')
+   nameItem.textContent = `New Favorite Movie: ${movie}`
+
+   const list = document.createElement('ul')
+   list.insertBefore(list, nameItem)
+
+   //details.insertBefore(list, nameItem)
 }
 
 cageMovies.addEventListener('submit', submitHander)
+cageMovies.addEventListener('submit', submitHandle)
