4385e47310ba2945b7531e2cab482585a40e3279
Pranav <pguru12@gmail.com>
2017-06-07 10:58:40 -0400

    List is working

--- index.js
+++ index.js
@@ -13,18 +13,19 @@ function submitHander(ev){
    nameItem.textContent = `Favorite Movie: ${movie}`
 
    const nameItem2 = document.createElement('li')
-   nameItem.textContent = `Second Favorite Movie: ${movie2}`
+   nameItem2.textContent = `Second Favorite Movie: ${movie2}`
 
    const list = document.createElement('ul')
    list.appendChild(nameItem)
    list.appendChild(nameItem2)
 
-    details.appendChild(boldName)
-   details.innerHTML=`
-   <ul>
-    <li>Movie: ${movie}</li>
-    <li>Movie: ${movie2}</li>
-   </ul>`
+   details.appendChild(list)
+   //list.appendChild(boldName)
+  // details.innerHTML=`
+  // <ul>
+//     <li>Movie: ${movie}</li>
+//     <li>Movie: ${movie2}</li>
+//    </ul>`
 }
 
 cageMovies.addEventListener('submit', submitHander)
