c958e3e724a2796ea72bd0e0e3ce611ee308944f
Pranav <pguru12@gmail.com>
2017-06-06 23:30:35 -0400

    Start listing

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cageMovies = document.querySelector('#cageMovies')
 function submitHander(ev){
     ev.preventDefault()
     const f = ev.target
+    const details=document.querySelector('#details')
+    const movie = f.cageMovies.value
+
+    details.innerHTML +='<strong>'+ movie+'</strong>'
 }
 
-cageMovies.addEventListener('submit')
+cageMovies.addEventListener('submit', submitHander)
