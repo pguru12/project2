0c3dafed6c9d576e1fb34cb4df3d95ced819c5c5
Pranav <pguru12@gmail.com>
2017-06-07 10:02:11 -0400

    Add another text box

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const cageMovies = document.querySelector('#cageMovies')
 
+function renderName(){
+    
+}
+
 function submitHander(ev){
     ev.preventDefault()
     const f = ev.target
@@ -9,12 +13,21 @@ function submitHander(ev){
 
    // const boldName = document.createElement('strong')
    // boldName.textContent = name
+   const nameItem = document.createElement('li')
+   nameItem.textContent = `Favorite Moie: ${movie}`
+
+   const nameItem2 = document.createElement('li')
+   nameItem.textContent = `Second Favorite Movie: ${movie2}`
+
+   const list = document.createElement('ul')
+   list.appendChild(nameItem)
+   list.appendChild(nameItem2)
 
    // details.appendChild(boldName)
    details.innerHTML=`
    <ul>
-    <li>Name: ${movie}</li>
-    <li>Name: ${movie2}</li>
+    <li>Movie: ${movie}</li>
+    <li>Movie: ${movie2}</li>
    </ul>`
 }
 
